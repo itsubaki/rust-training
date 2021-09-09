97855ce7388c94c2d8f92cd32265c922df32e53f
itsubaki <1759459+itsubaki@users.noreply.github.com>
2021-09-09 12:43:26 +0900

    Add bios bootimage

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 #![no_std]
 #![no_main]
-#![feature(abi_efiapi)]
 
-extern crate uefi_services;
+use core::panic::PanicInfo;
 
-use core::fmt::Write;
-use uefi::prelude::*;
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
 
-#[entry]
-fn efi_main(_image: Handle, st: SystemTable<Boot>) -> Status {
-    st.stdout().reset(false).unwrap_success();
-    writeln!(st.stdout(), "Hello, World!").unwrap();
-
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
     loop {}
 }
